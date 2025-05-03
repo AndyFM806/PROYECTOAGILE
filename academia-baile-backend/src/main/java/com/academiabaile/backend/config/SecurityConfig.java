c8f7171b7d9d6312eef64b9835a0d009954ea05d
AndyFM806 <leefernandez1245@gmail.com.>
2025-05-03 10:11:21 -0500

    config2

--- academia-baile-backend/src/main/java/com/academiabaile/backend/config/SecurityConfig.java
+++ academia-baile-backend/src/main/java/com/academiabaile/backend/config/SecurityConfig.java
@@ -2,17 +2,23 @@ package com.academiabaile.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors(cors -> cors.disable())
             .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+            
+            .formLogin(login -> login.disable())
+            .httpBasic(basic -> basic.disable());
+
         return http.build();
     }
 }
