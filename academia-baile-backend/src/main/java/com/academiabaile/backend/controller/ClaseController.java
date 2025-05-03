fea656c935467d87ebf3c16cf1fe9b5159081284
AndyFM806 <leefernandez1245@gmail.com.>
2025-05-03 11:31:15 -0500

    CAMBIOS

--- academia-baile-backend/src/main/java/com/academiabaile/backend/controller/ClaseController.java
+++ academia-baile-backend/src/main/java/com/academiabaile/backend/controller/ClaseController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/clases")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://timbatumbao.onrender.com")
 public class ClaseController {
 
     @Autowired
--- academia-baile-backend/src/main/java/com/academiabaile/backend/controller/ClienteController.java
+++ academia-baile-backend/src/main/java/com/academiabaile/backend/controller/ClienteController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/clientes")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://timbatumbao.onrender.com")
 public class ClienteController {
 
     @Autowired
