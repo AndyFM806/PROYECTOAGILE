20aa0a2f51195e61b0906349748bde919eff7c28
AndyFM806 <leefernandez1245@gmail.com>
2025-06-19 11:05:20 -0500

    WEBCONFIG

--- academia-baile-backend/src/main/java/com/academiabaile/backend/config/WebConfig.java
+++ academia-baile-backend/src/main/java/com/academiabaile/backend/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
+                registry.addMapping("/**")
                         .allowedOrigins(
                             "https://timbatumbao-front.onrender.com",
                             "http://localhost:5500"
